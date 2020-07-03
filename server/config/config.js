ed9ca0c3da28d1dc7b7560abfc811ebdf5ce8448
Erick Bolivar Tapia Ramirez <52500251+Bolo10@users.noreply.github.com>
2020-07-03 17:28:28 -0500

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:TmUywo6XgSkawf@cafe.3ub6d.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://cafe-user:TmUywo6XgSkawf@cafe.3ub6d.mongodb.net/cafe'//en la parte final asegurarse que tenga el nombre correcto de la base
 }
 
 process.env.URLDB = urlDB;
